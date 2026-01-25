7c7b25695e26a3cee8bba7f8399dc762d7da9c3f
Dilioni <ndanzidilioni@gmail.com>
2026-01-25 09:35:56 +0300

    Implement Eslint and Prettier

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}...`);
-})
+  console.log(`Listening on http://localhost:${PORT}...`);
+});
